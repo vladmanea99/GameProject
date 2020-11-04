fdc618c239ea2079289eb0424646f9162b8923c8
vladmanea99 <vladaron123@gmail.com>
2020-11-04 12:29:55 +0200

    Created scripts for patrolling and visual searching

---
+++ 2DPlatformer/Assets/Scripts/EnemyPatroll.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatroll : MonoBehaviour
+{
+    [SerializeField] List<Vector3> points;
+    [SerializeField] float speed;
+    [SerializeField] float waitTime;
+    bool hasToWait = false;
+    float currentWaitedTime;
+    Rigidbody2D rb2D;
+    Vector3 destinationPoint;
+    // Start is called before the first frame update
+
+    // When neccesary I just flip the entire game object on the OX axis to go left or right on the map
+    void Flip()
+    {
+        Vector3 newScale = transform.localScale;
+        newScale.x *= -1;
+        transform.localScale = newScale;
+    }
+
+    // Entity has to wait a certain amount of time until it can move
+    void Wait()
+    {
+        currentWaitedTime += Time.deltaTime;
+        if (currentWaitedTime > waitTime)
+        {
+            hasToWait = false;
+        }
+    }
+
+    // Give the object a destination to go to (usually it should be on same OY axis, just x to differ
+    void WalkTo(Vector3 destination)
+    {
+        Vector3 direction = destination - transform.position;
+        if (Mathf.Sign(direction.normalized.x) != Mathf.Sign(transform.localScale.x))
+        {
+            Flip();
+        }
+        rb2D.velocity = (direction.normalized * Time.deltaTime * speed);
+    }
+
+    // Find the next waypoint the List of waypoints 
+    Vector2 FindNextWaypoint(Vector3 currentDestination)
+    {
+        int indice = points.IndexOf(currentDestination);
+
+        if (indice == points.Count - 1)
+        {
+            return points[0];
+        }
+        else
+        {
+            return points[indice + 1];
+        }
+    }
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+
+        if (points.Count > 1)
+        {
+            destinationPoint = points[0];
+        }    
+    }
+
+    private void Update()
+    {
+        if (hasToWait)
+        {
+            Wait();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!hasToWait)
+        {
+            // If it's not close enough to the place it has to go, just make it walk for more time
+            if (Vector3.Distance(transform.position, destinationPoint) > 0.1)
+            {
+                WalkTo(destinationPoint);
+            }
+            // If it reached that place, it must wait a few seconds and then go to the next waypoint
+            else
+            {
+                hasToWait = true;
+                currentWaitedTime = 0;
+                rb2D.velocity = Vector2.zero;
+                destinationPoint = FindNextWaypoint(destinationPoint);
+            }
+        }
+        
+    }
+}
---
+++ 2DPlatformer/Assets/Scripts/EnemyVisionCollider.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVisionCollider : MonoBehaviour
+{
+    // Number of rays that should search to see if the enemy has clear sight of the player
+    [SerializeField] uint numberOfRays;
+
+    private CircleCollider2D circleCollider2D;
+    private Rigidbody2D rb2D;
+    private RaycastHit2D raycastHit2D;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Circle collider is used for the vision of the enemy
+        circleCollider2D = GetComponent<CircleCollider2D>();
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            // Need to find the number of the layer for Enemy and just say, search for every layer, other than it, thus the ~
+            int layerMask = ~(1 << LayerMask.NameToLayer("Enemy"));
+            Debug.Log("Player is in range");
+            Vector3 direction;
+            Vector3 startingRayPosition = transform.position + new Vector3(0, transform.localScale.y, 0);
+            // For each ray we see if it hits the player directly or not
+            for (int i = 0; i < numberOfRays; i++)
+            {
+                // The direction starts from the middle of the box of the enemy and towards different parts of the player collider
+                direction = (collision.transform.position + (i + 1) * collision.transform.localScale / numberOfRays) - startingRayPosition;
+                raycastHit2D = Physics2D.Raycast(startingRayPosition, direction.normalized, Mathf.Infinity, layerMask);
+                if (raycastHit2D.collider && raycastHit2D.collider.tag == "Player")
+                {
+                    Debug.Log("I found the player");
+                    break;
+                }
+            }
+        }
+        
+    }   
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            Debug.Log("Player is out of range");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
