457904579cf12c959de33f455e6acea19609833f
vladmanea99 <vladaron123@gmail.com>
2020-12-21 16:50:29 +0200

    Camera follows player
    Added z value to camera movemennt script
    
    Added camera follow up

---
+++ 2DPlatformer/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float smoothSpeed = 0.01f;
+    [SerializeField] Vector3 offset;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+}
