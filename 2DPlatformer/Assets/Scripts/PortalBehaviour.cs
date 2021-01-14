e21cc89ff7cfac25da365891dedc05633a6500dc
Bia103 <48888793+Bia103@users.noreply.github.com>
2021-01-14 21:22:34 +0200

    menu + pause + portal

---
+++ 2DPlatformer/Assets/Scripts/PortalBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PortalBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] int loadScene;
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        var player = collision.collider.GetComponent<PlayerBehaviour>();
+        if(player){
+
+            SceneManager.LoadScene(loadScene);
+        }
+    }
+}
