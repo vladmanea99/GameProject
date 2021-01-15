0fc13347a88bf8b56b34c9d28beb3ab6341200c1
Bia103 <48888793+Bia103@users.noreply.github.com>
2021-01-15 23:54:43 +0200

    legaturi portale menu

--- 2DPlatformer/Assets/Scripts/MainMenu.cs
+++ 2DPlatformer/Assets/Scripts/MainMenu.cs
@@ -9,6 +9,18 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+     public void LoadLevel1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
     public void QuitGame()
     {
         Debug.Log("Quit my friend");
