2bf2c09beed378d97abde2ab9b603d4d96afb9b6
KapkanMT <mariusluis59@yahoo.com>
2021-01-15 20:41:18 +0200

    Shooting Animation.

--- 2DPlatformer/Assets/Scripts/EnemyProjectileBehaviour.cs
+++ 2DPlatformer/Assets/Scripts/EnemyProjectileBehaviour.cs
@@ -8,12 +8,14 @@ public class EnemyProjectileBehaviour : MonoBehaviour
     public float Speed = 5f;
     public float lifetime = 5.0f;
     public float x;
-    private void Start() {
-        Destroy (gameObject, lifetime);
+    private void Start()
+    {
+        Destroy(gameObject, lifetime);
         GameObject Enemy = GameObject.Find("Enemy");
         EnemyPatroll enemyScript = Enemy.GetComponent<EnemyPatroll>();
-        //Vector3 newScale = transform.localScale;
         x = enemyScript.getCScale();
+        //Vector3 newScale = transform.localScale;
+
     }
 
     // Update is called once per frame
@@ -30,12 +32,11 @@ public class EnemyProjectileBehaviour : MonoBehaviour
             transform.position += -transform.right * Time.deltaTime * Speed;
         }
     }
-
     void OnCollisionEnter2D(Collision2D collision)
     {
         var player = collision.collider.GetComponent<PlayerBehaviour>();
         //Debug.Log("Intra aici");
-        if(player)
+        if (player)
         {
             player.TakeHit(1);
             Destroy(gameObject);
