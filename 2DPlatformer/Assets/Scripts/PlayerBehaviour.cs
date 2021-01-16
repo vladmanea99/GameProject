50f46e33829cc24aa3244da9b7ea2ed87f596188
vladmanea99 <vladaron123@gmail.com>
2021-01-16 10:01:31 +0200

    Added deathzone and enemies on fst level

--- 2DPlatformer/Assets/Scripts/PlayerBehaviour.cs
+++ 2DPlatformer/Assets/Scripts/PlayerBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerBehaviour : MonoBehaviour
 {
@@ -18,12 +19,20 @@ public class PlayerBehaviour : MonoBehaviour
     {
         
     }
+
+    public void Die()
+    {
+        Hitpoints = 0;
+        Destroy(gameObject);
+        SceneManager.LoadScene(0);
+    }
+
     public void TakeHit(float damage)
     { 
         Hitpoints -= damage;
         Healthbar.SetHealth(Hitpoints, MaxHitpoints);
         if(Hitpoints <= 0){
-            Destroy(gameObject);
+            
         }
     }
 }
